1304b2f36e063ba8fa5aea712554f99d2f407cc0
maonguyen8x <nguyenmao.2912@gmail.com>
2022-08-01 22:51:51 +0700

    Using the Spotify API

--- app/spotify/views.py
+++ app/spotify/views.py
@@ -1,10 +1,12 @@
+from urllib import response
 from django.shortcuts import render, redirect
 from .credentials import REDIRECT_URL, CLIENT_ID, CLIENT_SECRET
 from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import is_spotify_authenticated, update_or_create_user_tokens
+from .util import *
+from api.models import Room
 
 
 class AuthURL(APIView):
@@ -48,7 +50,23 @@ def spotify_callback(request, format=None):
 
 
 class IsAuthenticated(APIView):
-    def get(self, request, format=None):
+    def get(self):
         is_authenticated = is_spotify_authenticated(
             self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.get('room_code')
+        room = Room.objects.filter(code=room_code)
+        if room.exists():
+            room = room[0]
+        else:
+            return Response({}, status == status.HTTP_404_NOT_FOUND)
+        host = room.host
+        endpoint = "player/currently-playing"
+        response = execute_spotify_api_request(host, endpoint)
+        print(response)
+
+        return Response(response, status=status.HTTP_200_OK)
