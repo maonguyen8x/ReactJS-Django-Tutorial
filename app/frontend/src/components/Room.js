6f79fe7dfe39de69caf8676b3403385df1758b4f
BE-HT_MaoNV <maonv@hybrid-technologies.vn>
2022-08-05 11:26:45 +0700

    Using the Spotify API

--- app/frontend/src/components/Room.js
+++ app/frontend/src/components/Room.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Button, Typography } from '@material-ui/core';
 import CreateRoomPage from './CreateRoomPage';
+import MusicPlayer from "./MusicPlayer";
 
 export default class Room extends Component {
     constructor(props) {
@@ -10,7 +11,8 @@ export default class Room extends Component {
             guestCanPause: false,
             isHost: false,
             showSettings: false,
-            spotifyAuthenticated: false
+            spotifyAuthenticated: false,
+            song: {},
         };
         this.roomCode = this.props.match.params.roomCode;
         this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
@@ -19,7 +21,16 @@ export default class Room extends Component {
         this.renderSettings = this.renderSettings.bind(this);
         this.getRoomDetails = this.getRoomDetails.bind(this);
         this.authenticateSpotify = this.authenticateSpotify.bind(this);
+        this.getCurrentSong = this.getCurrentSong.bind(this);
         this.getRoomDetails();
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.getCurrentSong, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     getRoomDetails() {
@@ -47,6 +58,7 @@ export default class Room extends Component {
             .then((response) => response.json())
             .then((data) => {
                 this.setState({ spotifyAuthenticated: data.status });
+                console.log(data.status);
                 if (!data.status) {
                     fetch('/spotify/get-auth-url')
                         .then((response) => response.json())
@@ -56,6 +68,21 @@ export default class Room extends Component {
                 }
             });
     }
+
+    getCurrentSong() {
+        fetch("/spotify/current-song")
+            .then((response) => {
+            if (!response.ok) {
+            return {};
+            } else {
+                return response.json();
+            }
+        })
+        .then((data) => {
+            this.setState({ song: data });
+            console.log(data);
+        });
+}
 
     leaveButtonPressed() {
         const requestOptions = {
@@ -116,31 +143,18 @@ export default class Room extends Component {
         return (
             <Grid container spacing={1}>
                 <Grid item xs={12} align="center">
-                    <Typography variant="h6" component="h6">
-                        Code: {this.roomCode}
+                    <Typography variant="h4" component="h4">
+                            Code: {this.roomCode}
                     </Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h6" component="h6">
-                        Votes: {this.state.votesToSkip}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h6" component="h6">
-                        Guest Can Pause: {this.state.guestCanPause.toString()}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h6" component="h6">
-                        Host: {this.state.isHost.toString()}
-                    </Typography>
-                </Grid>
-                {this.state.isHost ? this.renderSettingsButton() : null}
-                <Grid item xs={12} align="center">
+                    </Grid>
+                    <MusicPlayer {...this.state.song} />
+                        {this.state.isHost ? this.renderSettingsButton() : null}
+                    <Grid item xs={12} align="center">
                     <Button
                         variant="contained"
                         color="secondary"
-                        onClick={this.leaveButtonPressed}>
+                        onClick={this.leaveButtonPressed}
+                    >
                         Leave Room
                     </Button>
                 </Grid>
